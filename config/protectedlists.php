582924c472deda9ced9711762f3a65bee9b625ab
Paul Schneider <paul.schneider@yepyep.co.uk>
2015-06-03 12:57:58 +0100

    adding list ID to protected lists array

--- config/protectedlists.php
+++ config/protectedlists.php
@@ -2,5 +2,6 @@
 
 # list ID's protected from user requests like DELETE or UPDATE
 return [
-	"8fc9f0de9ac96fe46aba7c9df93d5471",
+	"8fc9f0de9ac96fe46aba7c9df93d5471", // production registrants
+	"05f975b8eb5a37a7c75523f957c086b6", // testing registrants
 ];
